d590e71dcbdef607fb5d7ea525f3d6ba39887c94
erdem <83398363+pars-erdem@users.noreply.github.com>
2025-06-19 22:04:13 +0300

    Update Program.cs

--- WinFormsAppOdevv/WinFormsAppOdevv/Program.cs
+++ WinFormsAppOdevv/WinFormsAppOdevv/Program.cs
@@ -2,16 +2,13 @@ namespace WinFormsAppOdevv
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
             try
             {
-                // To customize application configuration such as set high DPI settings or default font,
-                // see https://aka.ms/applicationconfiguration.
+
                 ApplicationConfiguration.Initialize();
                 DatabaseHelper.InitializeDatabase();
 
